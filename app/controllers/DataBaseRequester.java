b44febb90bab6b4f37798d17f0a24396eecf1b92
Alex Walls <idrivebears@gmail.com>
2015-11-07 15:30:09 -0600

    moded routes, Databaserequester

--- app/controllers/DataBaseRequester.java
+++ app/controllers/DataBaseRequester.java
@@ -2,33 +2,35 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import java.util.ArrayList;
 import models.*;
 
 public class DataBaseRequester extends Controller {
 
-    public static ArrayList<Event> eventList(int userID) {
-        ArrayList<Event> listOfEvents;
-
-
+    public Result eventList(int userID) {
+        ArrayList<Event> listOfEvents = null;
         return ok(play.libs.Json.toJson(listOfEvents));
     }
 
-    public static Event eventProfile(int userID, int eventID) {
-
+    public Result eventProfile(int userID, int eventID) {
+        Event eventFetched = null;
+        return ok(play.libs.Json.toJson(eventFetched));
     }
 
-    public static User userProfile(int userID) {
-
+    public Result userProfile(int userID) {
+        User userFetched = null;
+        return ok(play.libs.Json.toJson(userFetched));
     }
 
-    public static Place placeProfile(int userID, int placeID) {
-
+    public Result placeProfile(int userID, int placeID) {
+        Place placeFetched = null;
+        return ok(play.libs.Json.toJson(placeFetched));
     }
 
-    public static Comment eventComments(int userID, int eventID) {
+    public Result eventComments(int userID, int eventID) {
+        ArrayList<Comment> commentsFetched = null;
 
+        return ok(play.libs.Json.toJson(commentsFetched));
     }
 
 }
