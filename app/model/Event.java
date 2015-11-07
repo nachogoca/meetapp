8f4b99a1bbdd2a2599627c4bd2e7529c61843658
Nacho <nacho.gon.car@gmail.com>
2015-11-07 13:24:02 -0600

    Event class

--- app/model/Event.java
+++ app/model/Event.java
@@ -1,9 +1,11 @@
-package models;
+import java.util.Date;
 
 public class Event {
 	public int IDEvent;
 	public int IDPlace; 
 	public int IDUser;
 	public String name;
-
+	public String description;
+	public Date date;
+	public String visibility;
 }
