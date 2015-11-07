251a7d6c67e06632544cd27076b2e36e2a772294
Alex Walls <idrivebears@gmail.com>
2015-11-07 14:52:56 -0600

    added databaserequester skele

---
+++ app/controllers/DataBaseRequester.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+public DataBaseRequester extends Controller {
+    public DataBaseRequester() {
+
+    }
+
+    public static ArrayList<Event> EventList(int userID) {
+
+    }
+
+    public static Event EventProfile(int userID, int eventID) {
+
+    }
+
+    public static User UserProfile(int userID) {
+
+    }
+
+    public static Place PlaceProfile(int userID, int placeID) {
+
+    }
+
+    public static Comment EventComments(int userID, int eventID) {
+        
+    }
+
+}
