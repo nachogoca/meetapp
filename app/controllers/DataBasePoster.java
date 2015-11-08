f34acebf6916a0d295abcc416a1f72d03b09fdda
Alex Walls <idrivebears@github.com>
2015-11-08 12:42:11 -0600

    Added databasepostter skele

---
+++ app/controllers/DataBasePoster.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import java.util.ArrayList;
+import models.*;
+import play.db.*;
+import java.sql.*;
+
+public class DataBasePoster extends Controller{
+    public DataBasePoster() {
+
+    }
+
+    public Result eventProfile() {
+        Event newEventProfile = new Event();
+
+
+    }
+
+
+
+}
