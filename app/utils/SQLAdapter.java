4c7471740645110651cf86188aa5f11e7976b214
Alex Walls <idrivebears@github.com>
2015-11-08 12:43:50 -0600

    Added SqlAdpater class for creating database requests

---
+++ app/utils/SQLAdapter.java
@@ -0,0 +1,18 @@
+package utils;
+
+import play.db.*;
+import java.sql.*;
+
+public class SQLAdapter {
+
+    Connection connection;
+
+    public SQLAdapter() {
+        try {
+
+        }
+        catch(Exception ex) {
+
+        }
+    }
+}
