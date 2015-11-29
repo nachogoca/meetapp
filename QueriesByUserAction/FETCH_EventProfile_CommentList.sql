2eddbba5894d77f0a943f7fdac456c912131afd3
nachogoca <nacho.gon.car@gmail.com>
2015-11-29 16:16:55 -0600

    Modify fetch eventprofile. Add procedure

--- QueriesByUserAction/FETCH_EventProfile_CommentList.sql
+++ QueriesByUserAction/FETCH_EventProfile_CommentList.sql
@@ -1,18 +1,28 @@
 -- FETCH_EVENT_PROFILE_COMMENT_LIST
 -- Fetch comments from event. 
--- Commentid, Comment text, commment creator and comment datetime
+-- Arguments: IDEvent
+-- Returns: Commentid, Comment text, commment creator and comment datetime
 
-SELECT	
-		Comment.IDComment,
-		Comment.CommentText,
-		Comment.CreationDate,
-		MA_User.Name,
-		MA_User.LastName
-FROM	
-		Comment
-		JOIN Event ON (Comment.IDEvent = Event.IDEvent)
-		JOIN MA_User ON (MA_User.IDUser = Comment.IDUserCreator)
-WHERE	
-		Event.IDEvent = 2
-ORDER BY
-		CreationDate
+CREATE PROCEDURE FETCH_EVENT_PROFILE_COMMENT_LIST
+(@IDEvent int)
+AS
+BEGIN
+
+	SELECT	
+			Comment.IDComment,
+			Comment.CommentText,
+			Comment.CreationDate,
+			MA_User.Name,
+			MA_User.LastName
+	FROM	
+			Comment
+			JOIN Event ON (Comment.IDEvent = Event.IDEvent)
+			JOIN MA_User ON (MA_User.IDUser = Comment.IDUserCreator)
+	WHERE	
+			Event.IDEvent = @IDEvent
+	ORDER BY
+			CreationDate
+
+END
+
+EXECUTE FETCH_EVENT_PROFILE_COMMENT_LIST 1
