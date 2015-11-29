a4b4283c95b6f21864a8f7103580baad2282bdd0
nachogoca <nacho.gon.car@gmail.com>
2015-11-29 14:31:44 -0600

    PUT Event working, with recommendation weight = 0

--- QueriesByUserAction/PUT_EventProfile.sql
+++ QueriesByUserAction/PUT_EventProfile.sql
@@ -2,9 +2,9 @@
 --	FacebookIDCreator varchar(50), EventName varchar(50), EventDescription varchar(50), 
 --	EventDateTime datetime, Visibility varchar(50), PlaceID int
 
-CREATE PROCEDURE PUT_EVENT_PROFILE_TEST
+CREATE PROCEDURE PUT_EVENT_PROFILE_TEST_2
 (@IDPlace			int,
-@IDCreatorUser		int, --change to facebook id
+@IDCreatorUser		int,
 @Name			varchar(50),
 @Description	varchar(500),
 @DateTime		datetime,
@@ -13,25 +13,47 @@ AS
 BEGIN
 
 	-- The last id inserted, necessary for other queries
-	DECLARE @NewIdentity INT
+	DECLARE @NewEventIdentity INT
 
 	-- Temporary friend list table
-	DECLARE @FriendListTable TABLE (IDFriendUser int)	
+	CREATE TABLE #TMP_FRIEND_LIST
+	(RowId			int,
+	IDFriendUser	int)
+
 
 	BEGIN TRANSACTION [TranEvent]
 
 	BEGIN TRY
-
-		
+			
 		-- Create new event
 		INSERT INTO Event(IDPlace, IDUser, Name, Description, DateTime, Visibility)
 		VALUES (@IDPlace, @IDCreatorUser, @Name, @Description, @DateTime, @Visibility)
 
 		-- The last identity value inserted
-		SELECT @NewIdentity = SCOPE_IDENTITY()
+		SELECT @NewEventIdentity = SCOPE_IDENTITY()
 
 		-- Get list of all friends. Visibility is not yet taken into account
-		SELECT * INTO @FriendListTable FROM FETCH_FRIEND_LIST_WITH_IDUSER(@IDCreatorUser)
+		INSERT INTO #TMP_FRIEND_LIST EXECUTE FETCH_FRIEND_LIST_WITH_ROWID @IDCreatorUser
+		
+		-- Variables to visit each friend
+		DECLARE @friendListCount INT 
+		SET @friendListCount  = (SELECT COUNT(*) FROM #TMP_FRIEND_LIST)
+
+		-- for each friend, create a recommendation and a relation between MA_User_Event
+		DECLARE @friendIndex int = 1
+		WHILE @friendIndex <= @friendListCount BEGIN
+			
+			-- Create recommendation, weigth = 0 				
+			INSERT INTO Recommendation (IDEvent, IDUser, Weight)
+			VALUES (@NewEventIdentity, (SELECT T.IDFriendUser FROM #TMP_FRIEND_LIST AS T WHERE T.RowId = @friendIndex), 0)
+
+			-- Create relation between user and event, with attendance = 0				
+			INSERT INTO MA_User_Event (IDEvent, IDUser, Attendance)
+			VALUES (@NewEventIdentity, (SELECT T.IDFriendUser FROM #TMP_FRIEND_LIST AS T WHERE T.RowId = @friendIndex), 0)
+
+			SET @friendIndex = @friendIndex + 1
+		END
+
 
 	COMMIT TRANSACTION [TranPlace]
 
@@ -43,5 +65,4 @@ BEGIN
 
 END
 
-INSERT INTO Event ( IDPlace, IDUser, Name, Description, DateTime, Visibility)
-VALUES (1101, 1, 1, 'NEW EVENT', 'DESCRIPTION', CONVERT(DATE, GETDATE()), 'Public')
+EXECUTE PUT_EVENT_PROFILE_TEST_2 2, 1, 'Event PUT_EVENT_PROFILE_TEST_2', 'Description PUT_EVENT_PROFILE_TEST_2', '2015-11-29', 'Public'
